7b3931d90041ac11ded26a2492e8a4ad4ffab158
Mark Ronquillo <markronquillo23@gmail.com>
2014-01-16 13:23:38 +0800

    initial commit -- add link to show and edit page

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,42 @@
+class PostsController < ApplicationController
+	def index 
+		@posts = Post.all
+	end
+
+	def new 
+		@post = Post.new # the reason why we add this line is because the error trapping will throw an error if @post is nil (@post.error.any?)
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def create
+		# render text: params[:post].inspect
+		@post = Post.new(post_params)
+		if @post.save
+			redirect_to action: :show, id: @post.id
+		else
+			render 'new'
+		end 
+	end
+
+	def edit
+		@post = Post.find(params[:id]) 
+	end
+
+	def update
+		@post = Post.find(params[:id]);
+
+		if @post.update(params[:post].permit(:title, :text))
+			redirect_to @post
+		else
+			render 'edit'
+		end 
+	end
+
+	private 
+		def post_params
+			params.require(:post).permit(:title, :text)
+		end
+end
